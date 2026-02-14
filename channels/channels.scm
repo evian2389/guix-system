3434bf5fa2ed1fdbbea9249de41bce82044500ee
orka <evian2389@gmail.com>
2026-02-14 22:52:05 +0900

    fix guix channel information

--- channels/channels.scm
+++ channels/channels.scm
@@ -1,7 +1,13 @@
 (list
   (channel
     (name 'guix)
-    (url "https://git.savannah.gnu.org/git/guix.git"))
+    (url "https://git.guix.gnu.org/guix.git")
+    (branch "master")
+    (introduction
+    (make-channel-introduction
+      "9edb3f66fd807b096b48283debdcddccfea34bad"
+    (openpgp-fingerprint
+      "BBB0 2DDF 2CEA F6A8 0D1D  E643 A2A0 6DF2 A33A 54FA"))))
   (channel
     (name 'rde)
     (url "https://git.sr.ht/~abcdw/rde")
