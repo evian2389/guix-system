9e9fe7c1f15467a1c3a231da61383266f38b6acd
orka <evian2389@gmail.com>
2026-02-16 02:50:06 +0900

    fix home definintion

--- src/raynet-guix/users/orka/home.scm
+++ src/raynet-guix/users/orka/home.scm
@@ -9,19 +9,25 @@
   #:use-module (raynet-guix home-services games)      ; For home-steam-service-type
   #:use-module (raynet-guix home-services emacs)      ; For home-emacs-config-service-type
   #:use-module (raynet-guix home-services finance)    ; For home-finance-service-type
-  #:use-module (srfi srfi-1))
+  #:use-module (srfi srfi-1)
+  #:use-module (raynet-guix users common))
 
-(home-environment
- (inherit common-home-environment)
- (packages
-  (append
-   %base-packages))
- (services
-  (list
-   (service home-games-service-type)
-   (service home-emacs-config-service-type)
-   (service home-finance-service-type)
-   (service home-dotfiles-service-type
-            (home-dotfiles-configuration
-             (source-directory ".")
-             (directories '("config/users/orka/files")))))))
+
+(define orka-home-environment
+  (home-environment
+   (inherit common-home-environment)
+   (packages
+    (append
+     (list) ;; Add specific orka packages here if needed
+     %base-packages))
+   (services
+     (append
+      (list
+        (service home-games-service-type)
+        (service home-emacs-config-service-type)
+        (service home-finance-service-type)
+        (service home-dotfiles-service-type
+                  (home-dotfiles-configuration
+                  (source-directory ".")
+                  (directories '("config/users/orka/files")))))
+      (home-environment-services common-home-environment)))))
